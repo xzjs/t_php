54a7853c44311dd480020908620fbfe6d2b1930a
xzjs <2310976219@qq.com>
2017-10-30 10:42:46 +0800

    关闭了csrf验证 创建了设备device表 将clock表的外键修改为device_id

--- database/migrations/2017_10_30_023830_create_devices_table.php
+++ database/migrations/2017_10_30_023830_create_devices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateClocksTable extends Migration
+class CreateDevicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateClocksTable extends Migration
      */
     public function up()
     {
-        Schema::create('clocks', function (Blueprint $table) {
+        Schema::create('devices', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('repeat_type');
-            $table->integer('duration');
-            $table->boolean('delete');//响铃后是否删除
-            $table->string('remark');
-            $table->dateTime('time');
-            $table->integer('device_id');
+            $table->integer('user_id');
+            $table->string('no');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateClocksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clocks');
+        Schema::dropIfExists('devices');
     }
 }
